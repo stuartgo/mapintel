304561b712a075f0a11d2e75d5d73c3a32cef03c
David Silva <david.silva.silvestre@gmail.com>
2020-12-11 22:33:46 +0000

    Create MiniSom2 class and restructure
    Split the som.py script into som_clustering.py for applying the SOM to
    a specific set of embedding vectors and som_tuning.py for applying
    hyperparameter tuning for a SOM with a specific set of embedding
    vectors.
    Make use of the MiniSom2 class which wraps the MiniSom class and
    provides an api more similar to sklearn and additional features such
    as the u_matrix method. In the future we can use this wrapper as a
    base for implementing additional features such as the multiprocessing
    training.

---
+++ src/cluster/som_tuning.py
@@ -0,0 +1,132 @@
+'''
+Self-Organising Maps
+
+Fit a SOM for a given model.
+
+Produces a .som file (saved in models/saved_models/)
+and generates a U-matrix plot (saved in src/cluster/).
+
+TODO:
+- improve u-matrix
+- update docstrings
+- add multiprocessing option to MiniSom2
+- change decay_function function parameter
+- implement the unfolding and fine-tuning training phases (maybe as a decay_function)
+
+Examples
+--------
+>>> # Fit a map for a given model, and save resulting SOM model
+>>> # in same directory, with same filename, but .som extension
+>>> python som.py ../../models/saved_models/TfidfVectorizer.joblib
+'''
+
+import logging
+import os
+
+import click
+from sklearn.model_selection import ParameterGrid
+from src import PROJECT_ROOT
+from src.cluster.som_clustering import (MiniSom2, check_som_exists, load_som,
+                                        save_som)
+from src.visualization.embedding_space import embedding_vectors, read_data
+
+
+def check_som_exists(fname, type):
+	"""Check if SOM exists before fitting it
+
+	Args:
+		fname ([type]): [description]
+		type ([type]): [description]
+
+	Raises:
+		ValueError: [description]
+
+	Returns:
+		[type]: [description]
+	"""
+	if type == "som":
+		fpath = os.path.join(model_dir, fname + '.som')
+	elif type == "viz":
+		fpath = os.path.join(fig_dir, fname + '.png')
+	else:
+		raise ValueError("type can only take 'viz' or 'som' values.")
+
+	# Check path existence
+	if os.path.exists(fpath):
+		return True
+	else:
+		return False
+
+
+@click.command()
+@click.argument('model_path', type=click.Path(exists=True))
+def main(model_path):
+	logger = logging.getLogger(__name__)
+	model_tag = os.path.splitext(os.path.basename(model_path))[0]
+
+	logger.info('Reading data...')
+    # Reading data into memory
+	_, train_docs, test_docs = read_data(data_file)
+	train_labels = train_docs['category']
+
+	logger.info('Obtaining document embeddings...')
+    # Obtain the vectorized corpus
+	vect_train_corpus, _ = embedding_vectors(
+        model_path, train_docs['prep_text'], test_docs['prep_text'])
+	input_len = vect_train_corpus.shape[1]
+
+	# Iterate over grid
+	for idx_mdl, params in enumerate(param_grid):
+		logger.info(f"Fitting SOM #{idx_mdl}")
+		logger.info("Fitting SOM with hyper-parameters: {}".format(params))
+
+		# Define SOM
+		som = MiniSom2(random_seed=0, verbose=True, input_len=input_len, **params)
+
+		if check_som_exists(str(som), "som"):
+			logger.info("SOM model {} already exists in disk...".format(str(som)))
+			if check_som_exists(str(som), "viz"):
+				logger.info("SOM figure {} already exists in disk...".format(str(som)))
+			else:
+				# Loading the SOM model
+				logger.info('Loading SOM model...')
+				som = load_som(os.path.join(model_dir, str(som) + '.som'))
+				# Plot and save the U-matrix
+				logger.info('Plotting and saving the U-matrix...')
+				som.u_matrix(vect_train_corpus, train_labels, fig_dir)
+		else:
+			# Fit SOM
+			logger.info('Fitting SOM model...')
+			som.fit(vect_train_corpus, train_labels)
+			# Plot and save the U-matrix
+			logger.info('Plotting and saving the U-matrix...')
+			som.u_matrix(vect_train_corpus, train_labels, fig_dir)
+			# Save the SOM fitted model
+			logger.info('Saving SOM model...')
+			outpath = os.path.join(model_dir, str(som) + '.som')
+			save_som(som, outpath)
+
+
+if __name__ == '__main__':
+	log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+	logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+	# Defining Paths
+	data_file = os.path.join(PROJECT_ROOT, "data", "processed", "newsapi_docs.csv")
+	model_dir = os.path.join(PROJECT_ROOT, "models", "saved_models")
+	fig_dir = os.path.join(PROJECT_ROOT, "models", "figures", "som")
+
+	# Hyperparameter grid
+	param_grid = ParameterGrid(
+        {	
+			'x': [30],
+			'y': [30],
+            'sigma': [1, 0.5],
+            'learning_rate': [1, 0.5],
+			'neighborhood_function': ['gaussian'],
+            'n_iter': [1000, 500, 100],
+			'topology': ['hexagonal']
+        }
+    )
+
+	main()

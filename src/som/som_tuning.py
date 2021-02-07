15686b2d35ad4ca89502c1988abd51849654360b
David Silva <david.silva.silvestre@gmail.com>
2021-02-07 23:30:49 +0000

    Rename src/cluster to src/som

--- src/som/som_tuning.py
+++ src/som/som_tuning.py
@@ -20,7 +20,7 @@ import numpy as np
 import sompy
 from sklearn.model_selection import ParameterGrid
 from src import PROJECT_ROOT
-from src.cluster.som_clustering import (check_som_exists, 
+from src.som.som_clustering import (check_som_exists, 
 									load_som,
 									save_som)
 from src.features.embedding_extractor import (embeddings_generator,

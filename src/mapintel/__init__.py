e2397963c73876f75f1c4d0773782280b498ba40
georgedouzas <gdouzas@icloud.com>
2022-10-25 10:05:46 +0300

    MNT Reformatted docstring

--- src/mapintel/__init__.py
+++ src/mapintel/__init__.py
@@ -1,7 +1,9 @@
 """
-mapintel package.
+Package mapintel.
 
-MapIntel is a system for acquiring intelligence from vast collections of text data by representing each document as a multidimensional vector that captures its own semantics. The system is designed to handle complex Natural Language queries and visual exploration of the corpus.
+MapIntel is a system for acquiring intelligence from vast collections of text data by representing each document as a 
+multidimensional vector that captures its own semantics. The system is designed to handle complex Natural 
+Language queries and visual exploration of the corpus.
 """
 
 from typing import List
